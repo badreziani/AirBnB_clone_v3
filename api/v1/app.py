e18ced3b80c0d77e0f2bd3a3e5564476aa8589c8
fuadhasen <fuadhas6634@gmail.com>
2024-08-05 10:42:33 +0530

    [add] task5: error handling

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
 """ Status of your API """
 import os
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix='/api/v1')
+
+
+@app.errorhandler(404)
+def error(exception=None):
+    """function to handle error """
+    return make_response(jsonify({'error': "Not found"}), 404)
 
 
 @app.teardown_appcontext
